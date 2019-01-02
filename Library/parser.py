f0f062b348394795ecee571354807700d6546249
Sam Adams <adams.2130@osu.edu>
2019-01-02 02:52:47 -0500

    Added type 1 & type 2 rtf parsing Fix errors with type 1 parsing. Does not completely parse out tags.

--- Library/parser.py
+++ Library/parser.py
@@ -1,7 +1,7 @@
 import re
 import csv
 import os
-
+from collections import defaultdict
 
 class FdsDict(dict):
     """
@@ -13,7 +13,7 @@ class FdsDict(dict):
         if self.get(key) == 0:
             self[key] = [item]
         else:
-            self[key].extend(item)
+            self[key].extend([item])
 
     def get(self, item):
         return dict.get(self, item, 0)
@@ -31,6 +31,7 @@ def output(text, filename, path):
         f.write(text)
 
 
+
 class ParseRtf(object):
     def __init__(self, output_directory = None):
         """
@@ -42,8 +43,10 @@ class ParseRtf(object):
         #identify all dates in factset date format
         self.dates = re.compile(r"([0-9]{1,2}) (January|February|March|April|May|June|July|August|September|October|November|December) (20[0-9]{2})")
         self.output_directory = os.path.join(output_directory)
+        self.files_output = defaultdict(int)
+        self.cache = None
 
-    def parse(self, rtf_text, filename):
+    def parse(self, rtf_text, filename, file = None):
         """
         Main entry point for class.
         Outputs file to location specified during the construction of the parse_rtf objet
@@ -57,8 +60,152 @@ class ParseRtf(object):
             #print('no date')
             return
         else:
-            filename = filename+date
+            try:
+                filename = filename+date
+            except TypeError:
+                print('halt')
             output(parsed_text, filename, self.output_directory)
+            self.files_output[file] += 1
+
+
+    @staticmethod
+    def _document_is_type_1(text):
+        """
+        Determines if a document contains images.
+        Type 1 RTF documents do not contain images
+        Type 2 RTF documents contain images
+        Type 1 documents have a line that has the string "Document " at the beginning of some line
+        :param text: A list of text
+        :return: Bool. True if a document is type 1, False if document is type 2.
+        """
+        type_1 = re.compile('Document ')
+        for line in text:
+            if type_1.match(line):
+                return True
+        return False
+
+    @staticmethod
+    def _end_of_type_1_document(text):
+        """
+        Determines if a line is the end of a document
+        All documents in FACTSET format have a line that begins with "Document "
+        :param text: Single line from the document
+        :return: Bool. True if end of document. False if not end of the document.
+        """
+        end_of_document = re.compile('Document ')
+        if end_of_document.match(text):
+            return True
+        else:
+            return False
+
+    def _parse_type_1_list(self, rtf_list, filename, file=None):
+        """
+        Parses a document without images
+        :param rtf_list: List of RTF text
+        :param filename: Filename of consolidated articles
+        :param file: OPTIONAL -- filename for diagnostics
+        :return: None -- Outputs to file
+        """
+        date = None
+        for line in rtf_list:
+            parsed_text = self._remove_type_1_tags(self._clean_url_field(self._create_newlines(line)))
+
+            #find the article date in the line
+            date_t = self._find_date(parsed_text)
+            if date_t:
+                date = date_t
+            #determine if we are at the end of the file, if not, store line in cache
+            #we should use the original line
+            if self._end_of_type_1_document(line):
+                try:
+                    filename_o = filename + date
+                except TypeError:
+                    print('halt')
+                self.cache += ('\n' + parsed_text)
+                output(self.cache, filename_o, self.output_directory)
+                self._clear_cache()
+                self.files_output[file] += 1
+            else:
+                self._update_cache(parsed_text)
+                continue
+
+
+
+
+
+
+    def _parse_type_2_list(self, rtf_list, filename, file=None):
+        """
+        Parses a document with images
+        :param rtf_list: List of RTF text
+        :param filename: Filename of consolidated articles
+        :param file: OPTIONAL -- filename for diagnostics
+        :return: None -- Outputs to file
+        """
+        for line in rtf_list:
+            parsed_text = self._remove_tags(self._clean_url_field(self._create_newlines(line)))
+            date = self._find_date(parsed_text)
+            if date is None:
+                self._update_cache(line)
+                continue
+            elif self._cache_not_empty(self.cache):
+                self.cache += (' ' + parsed_text)
+            try:
+                filename_o = filename+date
+            except TypeError:
+                print('halt')
+            output(self.cache, filename_o, self.output_directory)
+            self._clear_cache()
+            self.files_output[file] += 1
+
+
+    def parse_list(self, rtf_list, filename, file=None):
+        """
+        Main entry point for class.
+        Parses a RTF file that is in a list format
+        Outputs file to location specified during the construction of the parse_rtf objet
+        :param rtf_list: List of RTF text
+        :param filename: Filename of consolidated articles
+        :param file: OPTIONAL -- filename for diagnostics
+        :return: None -- Outputs to file
+        """
+        #determine document type:
+        if self._document_is_type_1(rtf_list):
+            self._parse_type_1_list(rtf_list, filename, file)
+        else:
+            self._parse_type_2_list(rtf_list, filename, file)
+
+        for line in rtf_list:
+            parsed_text = self._remove_tags(self._clean_url_field(self._create_newlines(line)))
+            date = self._find_date(parsed_text)
+            if date is None:
+                self._update_cache(line)
+                continue
+            elif self._cache_not_empty(self.cache):
+                self.cache += (' ' +parsed_text)
+            try:
+                filename_o = filename+date
+            except TypeError:
+                print('halt')
+            output(self.cache, filename_o, self.output_directory)
+            self._clear_cache()
+            self.files_output[file] += 1
+
+
+    def _clear_cache(self):
+        self.cache = None
+
+    @staticmethod
+    def _cache_not_empty(cache):
+        if cache is None:
+            return False
+
+
+    def _update_cache(self, line):
+        if self.cache is None:
+            self.cache = line
+        else:
+            self.cache += (' ' + line)
 
     def _find_date(self, rtf_text):
         """
@@ -100,6 +247,19 @@ class ParseRtf(object):
                       }
         return month_dict[month]
 
+    @staticmethod
+    def _remove_type_1_tags(rtf_text):
+        brackets = re.compile(r"[{}]")
+        headers = re.compile(r"\\f[2-6]")
+        bold = re.compile(r"\\b[0-3]?")
+        font = re.compile(r"\\fcharset0 .*?(?= |\\|;|\n);?")
+        remainder = re.compile(r"(\\).*?(?=\\| |;|\n);?")
+        rtf_text = headers.sub('',rtf_text)
+        rtf_text = bold.sub('',rtf_text)
+        rtf_text = font.sub('',rtf_text)
+        rtf_text = remainder.sub('',rtf_text)
+        rtf_text = brackets.sub('', rtf_text)
+        return rtf_text
 
     @staticmethod
     def _create_newlines(rtf_text):
@@ -153,6 +313,12 @@ class ParseRtf(object):
         if r'\par' in line[0:4]:
             return True
 
+    @staticmethod
+    def remove_images(line):
+        if ' ' not in line:
+            return False
+        else:
+            return line
 
 class IdentifyFilename(object):
     def __init__(self, filenames_location):
@@ -171,11 +337,16 @@ class IdentifyFilename(object):
         filename = filename[0:-4]
         parsed_filename = filename.split('_')
         if len(parsed_filename) == 1:
-            return self._construct_return_dictionary(parsed_filename[0], self.filename_dict)
+            t = self._construct_return_dictionary(parsed_filename[0], self.filename_dict)
         elif parsed_filename[1].isdigit():
-            return self._construct_return_dictionary(parsed_filename[0], self.filename_dict)
+            t = self._construct_return_dictionary(parsed_filename[0], self.filename_dict)
         else:
-            return self._construct_return_dictionary(parsed_filename[0], self.filename_dict, parsed_filename[1])
+            t = self._construct_return_dictionary(parsed_filename[0], self.filename_dict, parsed_filename[1])
+        return self._sanitize_filename(t)
+
+    @staticmethod
+    def _sanitize_filename(filename):
+        return re.sub(r'[/\\]',r'',filename)
 
     @staticmethod
     def _construct_return_dictionary(key, dictionary, second_key=None):
@@ -194,14 +365,17 @@ class IdentifyFilename(object):
             )
         else:
             for dict_list in td:
-                if dict_list['tic'] == second_key:
-                    return '{}_{}_{}_{}_{}_'.format(
-                        dict_list['conm'],
-                        dict_list['gvkey_chr'],
-                        key,
-                        dict_list['cusip'],
-                        dict_list['tic']
-                    )
+                try:
+                    if dict_list['tic'] == second_key:
+                        return '{}_{}_{}_{}_{}_'.format(
+                            dict_list['conm'],
+                            dict_list['gvkey_chr'],
+                            key,
+                            dict_list['cusip'],
+                            dict_list['tic']
+                        )
+                except TypeError:
+                    print('ERROR')
 
 
 
--- main.py
+++ main.py
@@ -1,19 +1,46 @@
 from Library.parser import ParseRtf, IdentifyFilename
 import os
 from tqdm import tqdm
+from collections import defaultdict
 
-identified_companies_csv = os.path.join('E:\\', 'Zahn', 'WORK_FILTER_FOR_ALL_SAS7BDAT.csv')
+identified_companies_csv = os.path.join('/home/pikakilla', 'Zahn', 'all.csv')
 identify_filenames = IdentifyFilename(identified_companies_csv)
 
-main_path = os.path.join('E:\\', 'Zahn', 'Articles_Sample')
-output_path = os.path.join('E:\\', 'Zahn', 'Output_Sample')
+main_path = os.path.join('/home/pikakilla', 'Zahn', 'Articles_2')
+output_path = os.path.join('/home/pikakilla', 'Zahn', 'Output_3')
 files = os.listdir(main_path)
 rtf_parser = ParseRtf(output_path)
 
+files_read = defaultdict(int)
+
 
 for file in tqdm(files):
+    #print(file)
     filename = identify_filenames.construct_output_filename(file)
+    files_read[file] += 1
+    if filename is None:
+        print('fn none')
     with open(os.path.join(main_path, file), 'r', errors='ignore') as f:
+        text = []
         for line in f:
-            if rtf_parser.identify_rtf_article(line):
-                rtf_parser.parse(line, filename)
+            if rtf_parser.remove_images(line):
+                text.append(line)
+        # for line in f:
+        #     if rtf_parser.identify_rtf_article(line):
+        rtf_parser.parse_list(text, filename, file=file)
+
+files_not_output_t = {}
+
+for key in files_read.keys():
+    files_not_output_t[key] = rtf_parser.files_output.get(key)
+
+files_not_output = {}
+for key in files_read.keys():
+    if files_not_output_t.get(key, 0) == 0:
+        files_not_output[key] = 0
+
+with open(os.path.join('/home/pikakilla/Zahn/files_output.txt'),'w') as f:
+    f.write(str(rtf_parser.files_output))
+
+with open(os.path.join('/home/pikakilla/Zahn/files_not_output.txt'),'w') as f:
+    f.write(str(files_not_output))
